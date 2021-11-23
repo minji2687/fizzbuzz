50ab66055dd01c69e60a554fe5f6eb9ad9b483b3
Jo Minji <mingi2687@gamil.com>
2021-11-23 15:14:43 +0900

    forEach로 바꿔서 만듬ㅎ

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,21 @@
-console.log("hi");
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0) {
+//     console.log("fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("fuzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
 
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
+let arr = new Array(100).fill(0);
+arr.forEach((el, index) => {
+  let num = index + 1;
+  if (num % 3 === 0) {
     console.log("fizz");
-  } else if (i % 5 === 0) {
+  } else if (num % 5 === 0) {
     console.log("fuzz");
   } else {
-    console.log(i);
+    console.log(num);
   }
-}
+});
