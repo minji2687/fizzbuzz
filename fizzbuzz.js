78a004e462e1fd34c616369e1499f2d7be6bb990
Jo Minji <mingi2687@gamil.com>
2021-11-23 14:58:41 +0900

    buzz출력

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,9 @@ console.log("hi");
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
+    // console.log("fizz");
+  } else if (i % 5 === 0) {
     console.log(i);
-    console.log("fizz");
+    console.log("fuzz");
   }
 }
