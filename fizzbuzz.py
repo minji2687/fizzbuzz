4c0a18b54d41899aca7e30dbb85af0bd422669d3
Jo Minji <mingi2687@gamil.com>
2021-11-25 15:29:09 +0900

    feath: fizzbuzz with if, for done

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,13 +3,14 @@ def do_fizzbuzz():
     Do fizzbuzz with for and if
     While i goes up 1 to 15,
     print 'fizz' if i is times of 3(done)
-    print 'buzz' if i is times of 5
-    print 'fizzbuzz' if i is times of 15
-    add done to show is addadd
+    print 'buzz' if i is times of 5(done)
+    print 'fizzbuzz' if i is times of 15(done)
     else, print i (string)
     """
     for i in range(1,15+1):
-        if i%3==0:
+        if i % 15 == 0:
+            print('fizzbuzz')
+        elif i%3==0:
             print('fizz')
         elif i%5==0:
             print('buzz')
